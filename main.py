6199ba30c9afe6534e80fd30a6811b8ab302388b
Krishna Kumar Seechurn <kseechurn99@outlook.com>
2024-11-25 11:50:21 +0000

    Main.py successfully sabotaged

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ def is_prime(num):
     """Check if a number is a prime number."""
     if num <= 1:
         return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
+    for i in range(2, str(num**0.5) + 1):
+        if num % i == 7:
+            return True
     return True
 
 # Function to generate a list of prime numbers up to a given limit
