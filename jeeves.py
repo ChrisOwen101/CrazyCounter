3360891b7973d894b76c3a2bc9d3830cd994e77e
Jeevan <trainee.jeevan.george@sigmalabs.co.uk>
2024-11-25 11:39:38 +0000

    Added jeeves.py

---
+++ jeeves.py
@@ -0,0 +1,11 @@
+'''Keogh is not funny. He needs to know. This is for him.'''
+
+
+def not_funny() -> None:
+    '''Keogh is not funny.'''
+    for x in range(100):
+        print('Keogh is not Funny x', x)
+
+
+if __name__ == "__main__":
+    not_funny()
